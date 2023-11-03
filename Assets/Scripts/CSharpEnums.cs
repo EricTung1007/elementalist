f54f9d752fa23094d7e53c3ef449bd029f91e60b
NR <b09201009@ntu.edu.tw>
2023-11-03 18:59:29 +0800

    Added some objects for element system.

---
+++ Assets/Scripts/CSharpEnums.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Element type ID for entities and skills
+enum Type
+{
+    none,
+    fire,
+    water,
+    grass
+}
+
+// Magic ID
+enum Magic
+{
+
+}
+
+// Status ID
+enum Status
+{
+
+}

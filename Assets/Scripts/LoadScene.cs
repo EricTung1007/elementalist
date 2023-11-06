5366a4bb944d9816b86f877dfc07081fb2c1a721
930419 <jolin930419@gmail.com>
2023-11-07 02:01:50 +0800

    Add some buttons

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -5,10 +5,10 @@ using UnityEngine.SceneManagement;
 
 public class LoadScene : MonoBehaviour
 {
+    [SerializeField] private string name;
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -22,5 +22,8 @@ public class LoadScene : MonoBehaviour
     }
     public void LoadTutorial(){
         SceneManager.LoadScene("Tutorial");
+    }
+    public void LoadStartMenu(){
+        SceneManager.LoadScene("StartMenu");
     }
 }

471e137299960aa802e7378bab702110feca4633
930419 <jolin930419@gmail.com>
2023-11-16 11:46:56 +0800

    basic setting for tutorial

--- Assets/Scripts/TutorialManager.cs
+++ Assets/Scripts/TutorialManager.cs
@@ -1,13 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Events;
 
 public class TutorialManager : MonoBehaviour
 {
-    public GameObject tutorialImage1;
-    public GameObject tutorialImage2;
-    public GameObject nextButton;
-    public GameObject previousButton;
+[SerializeField] private UnityEvent modifyActive;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,18 +17,5 @@ public class TutorialManager : MonoBehaviour
     void Update()
     {
         
-    }
-    public void Next(){
-        tutorialImage1.SetActive(false);  
-        tutorialImage2.SetActive(true);      
-        nextButton.SetActive(false); 
-        previousButton.SetActive(true);    
-    }
-
-     public void Previous(){
-        tutorialImage2.SetActive(false);  
-        tutorialImage1.SetActive(true);      
-        nextButton.SetActive(true); 
-        previousButton.SetActive(false);    
-    }  
+    } 
 }

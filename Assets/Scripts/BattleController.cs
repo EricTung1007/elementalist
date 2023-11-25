84f6cac291714b4668de0fe5dcebaed61bcda39f
NR <b09201009@ntu.edu.tw>
2023-11-25 14:10:31 +0800

    commited for pulling

--- Assets/Scripts/BattleController.cs
+++ Assets/Scripts/BattleController.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using Unity.VisualScripting;
 using UnityEditor;
-using UnityEditor.ShaderGraph.Internal;
+//using UnityEditor.ShaderGraph.Internal;
 using UnityEngine;
 using UnityEngine.Events;
 

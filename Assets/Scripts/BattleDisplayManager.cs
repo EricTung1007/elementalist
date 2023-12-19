7c197ca6f89656bb6c4fa358a34f3f0a9afe0723
jerrymk <jerrymklu@gmail.com>
2023-12-20 01:17:50 +0800

    webgl build

--- Assets/Scripts/BattleDisplayManager.cs
+++ Assets/Scripts/BattleDisplayManager.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class BattleDisplayManager : MonoBehaviour
 {
